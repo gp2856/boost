da7ac735f9c8968fa66b253ba6e5f9b3090ccf0c
GP <gp2856@users.noreply.github.com>
2022-07-05 13:33:38 -0500

    Implemented basic thrust and rotation

---
+++ boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float thrustSpeed = 1000.0f;
+    [SerializeField] float rotationSpeed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(Vector3.back);
+        }
+    }
+
+    private void ApplyRotation(Vector3 direction)
+    {
+        transform.Rotate(direction * rotationSpeed * Time.deltaTime);
+    }
+}
