270505594554f880f39bcc28f848c61560ff813b
GP <gp2856@users.noreply.github.com>
2022-07-06 11:28:20 -0500

    Added functionality to advance to the next level and loop back to the first

--- boost/Assets/Scripts/CollisionHandler.cs
+++ boost/Assets/Scripts/CollisionHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-
     void OnCollisionEnter(Collision other) 
     {
         switch(other.gameObject.tag)
@@ -14,7 +13,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This object is friendly");
                 break;
             case "Finish":
-                Debug.Log("You finished the level");
+                AdvanceLevel();
                 break;
             default:
                 Debug.Log("Sorry you crashed into something");
@@ -27,5 +26,22 @@ public class CollisionHandler : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void AdvanceLevel()
+    {
+        // Get Index of the next level
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        
+        // If the next Scene index is not greater than the number of scenes, load it
+        // otherwise, load the first level
+        if (nextSceneIndex < SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
